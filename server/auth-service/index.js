6f8a3800df0e42a20298614ceea1bfa52fc8bc3c
nithin-daniel <nithindaniel2018@gmail.com>
2025-12-17 23:05:19 +0530

    feat: enhance CORS configuration for auth and URL services

--- server/auth-service/index.js
+++ server/auth-service/index.js
@@ -18,7 +18,12 @@ connectDB();
 connectRabbitMQ();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
